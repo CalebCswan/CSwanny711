8c43c5359549d9ef1272f03f4ede4fb07228e0ff
CalebCswan <arachnidmale@outlook.com>
2024-05-20 09:59:33 -0500

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,24 @@
+
+
+console.log("Thank you for your email, Amir!");
+
+ 
+let responseBack = prompt("Would you like to play a game of chess?");
+
+if (responseBack !== "yes") {
+    console.log("I don't know you. You are a scammer. Please make better life choices and stop ruining India's reputation.");
+}
+
+if (responseBack == "yes") {
+    console.log("Great! It's your move.");
+}
+
+let moves = 0;
+
+let scammerChoice = ["e3", "e4", "f3", "c3"]; 
+let myChoices = ["e6", "e5", "f6", "c5"];
+
+if (scammerChoice = "e4"); {
+    console.log("c5");
+}
+
